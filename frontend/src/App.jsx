feabe451ac46cd945bcd5aa475ad45a230948816
dooishe <tugievzx@mail.ru>
2025-09-02 18:42:35 +0300

    Split Header into a component and use Link from react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage.jsx";
 import CheckoutPage from "./pages/CheckoutPage/CheckoutPage.jsx";
 import OrdersPage from "./pages/Orders/OrdersPage.jsx";
+import TrackingPage from "./pages/Tracking/TrackingPage.jsx";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
       <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
     </Routes>
   );
 }
--- frontend/src/pages/Orders/OrdersPage.jsx
+++ frontend/src/pages/Orders/OrdersPage.jsx
@@ -1,4 +1,6 @@
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import Header from "../../components/Header/Header";
 import "./OrdersPage.css";
 function OrdersPage() {
   useEffect(() => {
@@ -6,38 +8,9 @@ function OrdersPage() {
   }, []);
   return (
     <>
-      <div className="header">
-        <div className="left-section">
-          <a href="/" className="header-link">
-            <img className="logo" src="images/logo-white.png" />
-            <img className="mobile-logo" src="images/mobile-logo-white.png" />
-          </a>
-        </div>
-
-        <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
-
-          <button className="search-button">
-            <img className="search-icon" src="images/icons/search-icon.png" />
-          </button>
-        </div>
-
-        <div className="right-section">
-          <a className="orders-link header-link" href="orders">
-            <span className="orders-text">Orders</span>
-          </a>
-
-          <a className="cart-link header-link" href="checkout">
-            <img className="cart-icon" src="images/icons/cart-icon.png" />
-            <div className="cart-quantity">3</div>
-            <div className="cart-text">Cart</div>
-          </a>
-        </div>
-      </div>
-
+      <Header />
       <div className="orders-page">
         <div className="page-title">Your Orders</div>
-
         <div className="orders-grid">
           <div className="order-container">
             <div className="order-header">
@@ -81,11 +54,11 @@ function OrdersPage() {
               </div>
 
               <div className="product-actions">
-                <a href="tracking.html">
+                <Link to="/tracking">
                   <button className="track-package-button button-secondary">
                     Track package
                   </button>
-                </a>
+                </Link>
               </div>
 
               <div className="product-image-container">
@@ -110,11 +83,11 @@ function OrdersPage() {
               </div>
 
               <div className="product-actions">
-                <a href="tracking.html">
+                <Link to="/tracking">
                   <button className="track-package-button button-secondary">
                     Track package
                   </button>
-                </a>
+                </Link>
               </div>
             </div>
           </div>
@@ -159,11 +132,11 @@ function OrdersPage() {
               </div>
 
               <div className="product-actions">
-                <a href="tracking.html">
+                <Link to="/tracking">
                   <button className="track-package-button button-secondary">
                     Track package
                   </button>
-                </a>
+                </Link>
               </div>
             </div>
           </div>
