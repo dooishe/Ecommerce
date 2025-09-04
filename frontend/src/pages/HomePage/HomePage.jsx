2bb2d0063c4c5e4ac4f1892451b85615f1db67e2
dooishe <tugievzx@mail.ru>
2025-09-04 14:18:17 +0300

    fetch productList from backend with async function getProductList from useEffect in HomePage component

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,18 +1,32 @@
 import { useFavicon, useTitle } from "../../hooks/usePageMeta";
 import Header from "../../components/Header/Header";
 import ProductCard from "./components/ProductCard/ProductCard.jsx";
-import { products } from "../../../starting code/data/products.js";
 import "./HomePage.css";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function HomePage() {
+  const [productsList, setProductsList] = useState(null);
   useTitle("Home");
   useFavicon("/favicons/home-favicon.png");
+  useEffect(() => {
+    try {
+      async function fetchProductList() {
+        const response = await fetch("http://localhost:3000/api/products");
+        const data = await response.json();
+        setProductsList(data);
+      }
+      fetchProductList();
+    } catch (er) {
+      console.log(er);
+    }
+  }, []);
   return (
     <>
       <Header />
       <div className="home-page">
         <div className="products-grid">
-          {products.map((product) => {
+          {productsList?.map((product) => {
             return <ProductCard key={product.id} product={product} />;
           })}
         </div>
